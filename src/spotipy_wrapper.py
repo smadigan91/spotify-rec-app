d69617c6ca90c01d3bf401461199a40415223d91
Sean <pipgargery@gmail.com>
2018-08-25 15:54:47 -0400

    document spotipy_wrapper methods

--- src/spotipy_wrapper.py
+++ src/spotipy_wrapper.py
@@ -24,13 +24,14 @@ class SpotipyWrapper:
 
     def __init__(self, access_token):
         self.sp = spotipy.Spotify(auth=access_token)
-        
+
     def get_targeted_recs(self, seed_track, rec_limit=limit, max_tracks_per_artist=None):
         """
-        :param seed_track:
-        :param rec_limit:
-        :param max_tracks_per_artist:
-        :return:
+        uses target_* for each attribute
+        :param seed_track: the track to generate recommendations from
+        :param rec_limit: max number of recommendations per track, up to 100
+        :param max_tracks_per_artist: max number of tracks per artist in recommendations
+        :return: a set of the recommended tracks
         """
         track_features = self.sp.audio_features(seed_track)[0]
         all_track_stats = {k: v for k, v in track_features.items() if (k in key_attrs or k in other_attrs)}
@@ -44,8 +45,15 @@ class SpotipyWrapper:
                 rec_tracks.remove(track)
         return set(rec_tracks)
 
-    # min/max the min/maxables, target everything else
     def get_mixed_recs(self, seed_track, variance, rec_limit=limit, max_tracks_per_artist=None):
+        """
+        max_*/min_* key attributes by +/- variance%, target_* everything else
+        :param seed_track: the track to generate recommendations from
+        :param variance: percentage to vary max/min by relative to base (e.g. 0.2 means +/-20% of the original value)
+        :param rec_limit: max number of recommendations per track, up to 100
+        :param max_tracks_per_artist: max number of tracks per artist in recommendations
+        :return: a set of the recommended tracks
+        """
         track_features = self.sp.audio_features(seed_track)[0]
         adv_track_stats = {k: v for k, v in track_features.items() if k in key_attrs}
         other_track_stats = {k: v for k, v in track_features.items() if k in other_attrs}
@@ -62,6 +70,13 @@ class SpotipyWrapper:
         return set(rec_tracks)
 
     def get_fuzzy_recs(self, seed_track, variance, rec_limit=limit, max_tracks_per_artist=None):
+        """
+        :param seed_track: the track to generate recommendations from
+        :param variance: percentage to vary max/min by relative to base (e.g. 0.2 means +/-20% of the original value)
+        :param rec_limit: max number of recommendations per track, up to 100
+        :param max_tracks_per_artist: max number of tracks per artist in recommendations
+        :return: a set of the recommended tracks
+        """
         # track_stats = self.sp.track(seed_track[0])
         # artist_uris = [artist['uri'] for artist in track_stats['artists']]
         # genres = self.sp.artists(artist_uris)['artists'][0]['genres'][:5]
@@ -78,8 +93,13 @@ class SpotipyWrapper:
                 rec_tracks.remove(track)
         return set(rec_tracks)
 
-    # assumes track and artist uri tuple passed in
     def get_songs_per_artist(self, rec_track_tuples_list, max_tracks_per_artist=None):
+        """
+        combs tracks to remove any more than max_tracks_per_artistt per artist
+        :param rec_track_tuples_list: list of tuples of track uris and their artist's uris
+        :param max_tracks_per_artist: max number of tracks per artist in recommendations
+        :return: a curated list of track uris
+        """
         artist_map = {}
         track_uris = []
         for track_artist_tuple in rec_track_tuples_list:
@@ -96,6 +116,7 @@ class SpotipyWrapper:
             track_uris.extend(tracks)
         return track_uris
 
+    # extracts recommended tracks given a spotipy response
     def extract_rec_tracks(self, recs, max_tracks_per_artist=None):
         if not max_tracks_per_artist:
             rec_tracks = [track['uri'] for track in recs['tracks']]
@@ -104,6 +125,7 @@ class SpotipyWrapper:
             rec_tracks = self.get_songs_per_artist(rec_tracks_tuples_list, max_tracks_per_artist)
         return rec_tracks
 
+    # given a spotipy response return a list of tuples of track uris and their artist's uris
     def extract_track_artist_tuples_list(self, recs):
         rec_tracks_tuples_list = [(track['uri'], track['artists'][0]['uri']) for track in recs['tracks']]
         return rec_tracks_tuples_list
@@ -115,6 +137,7 @@ class SpotipyWrapper:
     def add_track_to_playlist(self, tracks, username, playlist_id):
         self.sp.user_playlist_add_tracks(username, playlist_id, tracks)
 
+    # creates a playlist given a set of track uris
     def create_playlist(self, track_uris):
         playlist_id = self.sp.user_playlist_create(USERNAME, playlist_name, public=True)['id']
 
@@ -125,6 +148,12 @@ class SpotipyWrapper:
             self.add_track_to_playlist(tracks=chunk, username=USERNAME, playlist_id=playlist_id)
 
     def create_similar_playlist(self, playlist_id, max_recs_per_seed=5, max_tracks_per_artist=None):
+        """
+        creates a playlist with tracks similar to those in a given playlist's
+        :param playlist_id: the id of the seed playlist
+        :param max_recs_per_seed: max number of recommended tracks per song on seed playlist
+        :param max_tracks_per_artist: max number of tracks per artist on target playlist
+        """
         playlist = self.sp.user_playlist_tracks(USERNAME, playlist_id, limit=100)
         playlist_tracks = [item['track']['uri'] for item in playlist['items']]
         rec_tracks = set()
@@ -160,6 +189,12 @@ class SpotipyWrapper:
     """
 
     def get_top_tracks(self, track_limit=50, time_range='medium_term'):
+        """
+        gets the top up to 50 tracks for the user
+        :param track_limit: number of tracks to fetch
+        :param time_range: short_term, medium_term, or long_term
+        :return: top track uris
+        """
         top_tracks = self.sp.current_user_top_tracks(limit=track_limit, time_range=time_range)
         top_uris = [track['uri'] for track in top_tracks['items']]
         return top_uris
@@ -173,6 +208,12 @@ class SpotipyWrapper:
             print('{:<5}{:<70}{}'.format(x, track_tuple[0], track_tuple[1]))
 
     def get_top_genres(self, artist_limit=50, time_range='medium_term'):
+        """
+        gets the user's most listened to artist's genres
+        :param artist_limit: number of artists to fetch
+        :param time_range: short_term, medium_term, or long_term
+        :return: the list of artist's genres
+        """
         top_artists = self.sp.current_user_top_artists(limit=artist_limit, time_range=time_range)
         genres = [artist['genres'] for artist in top_artists['items']]
         return genres
@@ -186,6 +227,11 @@ class SpotipyWrapper:
             print('{:<5}{:<50}{}'.format(x, track_tuple[0], track_tuple[1]))
 
     def get_average_user_track_data(self, top_uris):
+        """
+        collectts and averages the average-able attributes of the given tracks, buckets the non-average-able ones
+        :param top_uris: top track uris
+        :return: a tuple of the averages and the buckets
+        """
         total_tracks = len(top_uris)
         average_map = {}
         top_map = {x: {} for x in non_divisible_attrs}
@@ -214,6 +260,12 @@ class SpotipyWrapper:
         return average_map, top_map
 
     def rank_genres(self, genre_lists, top=5):
+        """
+        derives the most listened to genres given a list of lists of genres
+        :param genre_lists: list of genres lists
+        :param top: number of genres to return
+        :return: the top genres
+        """
         genre_map = {}
         for genre_list in genre_lists:
             for genre in genre_list:
@@ -223,12 +275,16 @@ class SpotipyWrapper:
                     genre_map[genre] = 1
         return sorted(genre_map, key=genre_map.__getitem__, reverse=True)[:top]
 
-    # needs to be reworked
-    # keep a mapping of genre bucket to set of stats, so averages are calculated on a per-bucket basis
-
+    # needs to be reworked; fringe music genres can tip the scales too heavily
     def create_average_top_playlist(self, top_genres, div_stats, non_div_stats, rec_limit=limit):
-        # given 5 top genres, averaged stats, and counting stats,
-        # get recommendations make playlist lol
+        """
+        creates a playlist based on the average stats from a user's top artists/genres
+        :param top_genres: most listened to genres
+        :param div_stats: the features that have been averaged
+        :param non_div_stats: the features that have been bucketed
+        :param rec_limit: max number of recommendations per track, up to 100
+        :return:
+        """
         targets = {f'target_{k}': v for k, v in div_stats.items()}
         targets.update({f'target_{k}': v[0] for k, v in non_div_stats.items()})
         rec_tracks = set()
